70aa1364afce50f319bfc63672665bdc5f636a4b
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2023-10-03 14:12:29 +0200

    Multiply integers added

--- epi_judge_cpp/int_as_array_multiply.cc
+++ epi_judge_cpp/int_as_array_multiply.cc
@@ -4,7 +4,27 @@
 using std::vector;
 vector<int> Multiply(vector<int> num1, vector<int> num2) {
   // TODO - you fill in here.
-  return {};
+  int n = num1.size();
+  int m = num2.size();
+  vector<int> result(n+m,0);
+  for (int i = n-1; i >= 0; --i) {
+    for (int j = m-1; j >=0; --j) {
+      int mul = (num1[i] - '0') * (num2[j] - '0');
+      int sum = mul + result[i + j + 1];
+
+      result[i + j] += sum / 10;
+      result[i + j + 1] = sum % 10; 
+
+    }    
+  }
+
+  int i = 0;
+  while (i < n +m -1 && result[i] == 0) {
+    i++;
+  }
+
+  vector<int> finalResult(result.begin() + i, result.end()); 
+  return finalResult;
 }
 
 int main(int argc, char* argv[]) {
---
+++ epi_judge_cpp/tempCodeRunnerFile.cc
@@ -0,0 +1,2 @@
+
+      result[i + j] += sum / 10;
