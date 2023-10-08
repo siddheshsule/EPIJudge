ccaad8f692a6cd5af7a80dd7ad182428963ef309
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2023-10-08 10:02:23 +0200

    New

--- epi_judge_cpp/int_as_array_multiply.cc
+++ epi_judge_cpp/int_as_array_multiply.cc
@@ -7,14 +7,15 @@ vector<int> Multiply(vector<int> num1, vector<int> num2) {
   const int sign = num1.front() < 0 ^ num2.front() < 0 ? -1 : 1;
   num1.front() = abs(num1.front()); 
   num2.front() = abs(num2.front());
-  int j = num2.size() - 1;
+  
 
   vector<int> result(num1.size() + num2.size() , 0); // Initializing a vector with number of zeroes equal to total sizes of two input vectorsa
   for (int i = num1.size() - 1 ; i >= 0; i--) {    
-    result[i + j + 1] += num1[i] * num2[j];
-    result[i + j] += result[i +j + 1] / 10;
-    result[i + j + 1] %= 10;    
-    j--;
+    for (int j = num2.size() - 1; j >= 0; j--) {
+      result[i + j + 1] += num1[i] * num2[j];
+      result[i + j] += result[i +j + 1] / 10;
+      result[i + j + 1] %= 10;    
+    }    
   }
 
   // Removing leading zeroes
